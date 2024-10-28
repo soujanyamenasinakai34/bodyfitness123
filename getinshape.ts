561cedefa32089ec89f3166ebc3e5ee5754ad6e6
soujanyamenasinakai34git <soujanyamenasinakai34@gmail.com>
2024-10-28 13:32:05 +0530

    project

---
+++ getinshape.ts
@@ -0,0 +1,9 @@
+function startAnimation(type) {
+    const animations = {
+        jumpingjacks: "Jumping Jack- Great for cardio!",
+        squats: "Squats - Excellent for lower body strength!",
+        plank: "Plank - Core strength builder!"
+    };
+    
+    alert(animations[type]);
+}
