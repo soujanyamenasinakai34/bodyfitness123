aa8e3553ead7af06b88c5badd82a8a9a8d17f45c
soujanyamenasinakai <soujanyamenasinakai34@gmail.com>
2024-10-28 16:39:02 +0530

    Delete getinshape.js

--- getinshape.js
+++
@@ -1,9 +0,0 @@
-function startAnimation(type) {
-    const animations = {
-        jumpingjacks: "Jumping Jack- Great for cardio!",
-        squats: "Squats - Excellent for lower body strength!",
-        plank: "Plank - Core strength builder!"
-    };
-    
-    alert(animations[type]);
-}
